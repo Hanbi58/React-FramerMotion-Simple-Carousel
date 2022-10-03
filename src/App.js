b11e4456116be669d57b44e18e4bc4c433026bf1
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-02 23:28:25 -0700

    simple carousel

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import { useState, useRef, useEffect } from "react";
 import images from "./images";
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
   return (
     <div className="App">
-      <motion.div className="carousel">
-        <motion.div className="inner-carousel">
+      <motion.div
+        className="carousel"
+        ref={carousel}
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
           {images.map((image) => (
-            <motion.div className="item">
+            <motion.div
+              className="item"
+              key={image}
+              whileHover={{ scale: 1.05 }}
+            >
               <img src={image} alt="" />
             </motion.div>
           ))}
